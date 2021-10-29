de54f3e65af5cb58452a1923b867b6d8fecddcd0
John Doe <geandro_a@hotmail.com>
2021-10-28 22:38:00 -0300

    component-entrar-cadastrar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { EntrarComponent } from './entrar/entrar.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
 
 
 @NgModule({
@@ -12,6 +14,8 @@ import { RodapeComponent } from './rodape/rodape.component';
     AppComponent,
     MenuComponent,
     RodapeComponent,
+    EntrarComponent,
+    CadastrarComponent,
   
   ],
   imports: [
