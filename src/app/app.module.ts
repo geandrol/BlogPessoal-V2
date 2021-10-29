10c8fb962ad2f26fe97df9a668745be75de1d410
John Doe <geandro_a@hotmail.com>
2021-10-28 22:27:58 -0300

    component-menu-rodape

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    RodapeComponent,
+  
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
