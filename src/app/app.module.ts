a38df064fbbbeb0b9ad29d4eaae7e4b4087aaaac
John Doe <geandro_a@hotmail.com>
2021-11-01 23:24:06 -0300

    model/login/cadastro e rotas

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { HttpClientModule } from '@angular/common/http'
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { EntrarComponent } from './entrar/entrar.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { InicioComponent } from './inicio/inicio.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    RodapeComponent,
+    EntrarComponent,
+    CadastrarComponent,
+    InicioComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
